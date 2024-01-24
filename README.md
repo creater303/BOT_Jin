06bb068a28c1ae9e6b580bf700ea599217713b07
email <jasonguomc@163.com>
2024-01-24 19:19:31 +0800

    重构此项目（使用telebot）

--- README.md
+++ README.md
@@ -3,10 +3,9 @@
 总喜欢在别人做出一些逆天事情时来一局
 低能
 就像个bot一样，于是就有了这么一个项目
-本项目中有一部分代码来自https://zhuanlan.zhihu.com/p/60933824
-                      https://www.npmjs.com/package/node-telegram-bot-api
-非常感谢知乎大神深红的分享
+、
 
 #依赖
-在使用本项目前需安装node-telegram-bot-api
-
+本项目基于telebot
+并对此做了一些汉化
+https://github.com/mullwar/telebot
--- index.js
+++ index.js
@@ -1,21 +1,28 @@
-const TelegramBot = require('node-telegram-bot-api');
-const Agent = require('socks5-https-client/lib/Agent');
+const TeleBot = require('telebot');
 
-const token = '填入你的token';
-const bot = new TelegramBot(token, {
-  polling: true,
-  request: { // 设置代理
-    agentClass: Agent,
-    agentOptions: {
-      socksPassword: '填入你的密码'
+const bot = new TeleBot({
+    token: TELEGRAM_BOT_TOKEN, // 这里需要填入从botfater处获取的token
+    polling: { // 使用polling方式换取tg消息
+        interval: 1000, // Optional. 检查是否有信息的频率 (in ms).
+        timeout: 0, // Optional.  (0 - short polling).
+        limit: 100, // Optional. 重试次数限制
+        retryTimeout: 5000, // Optional. 重连timeout (in ms).
+        proxy: 'http://username:password@yourproxy.com:8080' // Optional. An HTTP proxy to be used.
+    },
+    webhook: { // Optional. 使用webhook方式更新消息.
+        key: 'key.pem', // Optional. 服务器公钥.
+        cert: 'cert.pem', // Optional. 服务器私钥
+        url: 'https://....', // 发送更新使用的HTTPS url
+        host: '0.0.0.0', // Webhook 服务器主机
+        port: 443, //服务器端口号
+        maxConnections: 40 // Optional. Maximum allowed number of simultaneous HTTPS connections to the webhook for update delivery
+    },
+    allowedUpdates: [], // Optional. List the types of updates you want your bot to receive. Specify an empty list to receive all updates.
+    usePlugins: ['askUser'], // Optional. 使用 plugins from pluginFolder.
+    pluginFolder: '../plugins/', // Optional. Plugin folder location.
+    pluginConfig: { // Optional. Plugin configuration.
+        // myPluginName: {
+        //   data: 'my custom value'
+        // }
     }
-  }
 });
-
-bot.onText(/\/低能/, function onLoveText(msg) {
-    bot.sendMessage(msg.chat.id, '又遇到了一个低能的人');
-  });
-
-  bot.onText(/\/hi/, function onLoveText(msg) {
-    bot.sendMessage(msg.chat.id, '又活着呢');
-  });
