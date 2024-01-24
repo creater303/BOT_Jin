d49262fa858dea0b8f11fe82f876f0c5df3b3328
email <jasonguomc@163.com>
2024-01-24 10:51:03 +0800

    bot_Jin v0.1 只有一个框架和QA

--- README.md
+++ README.md
@@ -3,5 +3,10 @@
 总喜欢在别人做出一些逆天事情时来一局
 低能
 就像个bot一样，于是就有了这么一个项目
-This bot is for telegram
+本项目中有一部分代码来自https://zhuanlan.zhihu.com/p/60933824
+                      https://www.npmjs.com/package/node-telegram-bot-api
+非常感谢知乎大神深红的分享
+
+#依赖
+在使用本项目前需安装node-telegram-bot-api
 
---
+++ index.js
@@ -0,0 +1,21 @@
+const TelegramBot = require('node-telegram-bot-api');
+const Agent = require('socks5-https-client/lib/Agent');
+
+const token = '填入你的token';
+const bot = new TelegramBot(token, {
+  polling: true,
+  request: { // 设置代理
+    agentClass: Agent,
+    agentOptions: {
+      socksPassword: '填入你的密码'
+    }
+  }
+});
+
+bot.onText(/\/低能/, function onLoveText(msg) {
+    bot.sendMessage(msg.chat.id, '又遇到了一个低能的人');
+  });
+
+  bot.onText(/\/hi/, function onLoveText(msg) {
+    bot.sendMessage(msg.chat.id, '又活着呢');
+  });
